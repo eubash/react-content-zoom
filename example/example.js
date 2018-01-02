beec3de4624ad437520d2bcf8835380990ce4ebb
Eubash <ssimplatina@gmial.com>
2018-01-02 16:04:06 +0100

    add usage example

---
+++ example/example.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ContentZoom from 'react-content-zoom';
+
+import '../css/main.css'
+
+/**
+ * Base component
+ *
+ * NOTE: this is simple example of usage
+ *
+ * */
+export default class extends React.Component {
+
+    render() {
+        return <div>
+            <h1>React Content Zoom example!</h1>
+
+            <div>
+                <ContentZoom zoomPercent={350} largeImageUrl="./static/img.jpg" imageUrl="./static/img_small.jpg"
+                              contentWidth={426} contentHeight={266}/>
+            </div>
+
+            <h2>Put some content inside!</h2>
+
+            <div>
+                <ContentZoom zoomPercent={350} largeImageUrl="./static/img.jpg" imageUrl="./static/img_small.jpg"
+                              contentWidth={426} contentHeight={266}>
+                    <h1 className="example-title">Put some content here...</h1>
+                </ContentZoom>
+            </div>
+        </div>;
+    }
+
+}
